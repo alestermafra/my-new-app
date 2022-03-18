0cb2fb840602519e2800ffe1fcedaa80d61ced68
Alester Mafra <alester.mafra@gmail.com>
2022-03-17 23:57:11 -0300

    Segunda página

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,7 @@ function Home() {
     return (
         <>
             <h1>Home</h1>
-            <Contador />
         </>
-    );
-}
-
-function Contador() {
-    const [count, setCount] = useState(1);
-
-    return (
-        <button onClick={() => setCount(count + 1)} style={{padding: '5px 15px'}}>{count}</button>
     );
 }
 
